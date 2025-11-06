62256f842918d30bccca39817ac6bbcfe4988db9
leblaluc <lucasleblanc30@gmail.com>
2025-11-05 19:09:28 -0500

    rev 1

---
+++ src/main/java/gitpractice/lastandfinalgitpractice/Lastandfinalgitpractice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gitpractice.lastandfinalgitpractice;
+
+/**
+ *
+ * @author lucas
+ */
+public class Lastandfinalgitpractice {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1");
+    }
+}
